881f29203492b639c76a9205b21f9ad482c52131
Nahid71 <nahidjahid8698@gmail.com>
2019-04-08 02:45:55 +0600

    last one

--- HealthNet/settings.py
+++ HealthNet/settings.py
@@ -85,7 +85,10 @@ LOGIN_URL = '/login/'
 STATIC_URL = '/static/'
 
 SESSION_COOKIE_SECURE = not DEBUG
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
